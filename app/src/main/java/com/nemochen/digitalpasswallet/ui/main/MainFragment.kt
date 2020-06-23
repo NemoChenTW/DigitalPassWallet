bd3e98d02367ea43aeb10efdb016482680e75c4b
NemoChen <nemochentw@gmail.com>
2020-06-23 11:37:52 +0800

    Setup data binding

--- app/src/main/java/com/nemochen/digitalpasswallet/ui/main/MainFragment.kt
+++ app/src/main/java/com/nemochen/digitalpasswallet/ui/main/MainFragment.kt
@@ -1,12 +1,13 @@
 package com.nemochen.digitalpasswallet.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.nemochen.digitalpasswallet.R
+import com.nemochen.digitalpasswallet.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -14,17 +15,20 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private lateinit var binding: MainFragmentBinding
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+        binding = MainFragmentBinding.inflate(inflater, container, false)
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
     }
 
 }
